d0ed4ad6ed64b91c15ea67fb4e6faabb15b11515
Darmo117 <darmo117@gmail.com>
2024-02-05 13:30:13 +0100

    add custom portal to city dimension

--- src/main/java/net/darmo_creations/infinitecity/InfiniteCity.java
+++ src/main/java/net/darmo_creations/infinitecity/InfiniteCity.java
@@ -3,6 +3,9 @@ package net.darmo_creations.infinitecity;
 import net.darmo_creations.infinitecity.blocks.*;
 import net.darmo_creations.infinitecity.chunk_gen.*;
 import net.fabricmc.api.*;
+import net.kyrptonaught.customportalapi.api.*;
+import net.kyrptonaught.customportalapi.portal.*;
+import net.minecraft.block.*;
 import net.minecraft.registry.*;
 import net.minecraft.util.*;
 
@@ -13,5 +16,12 @@ public class InfiniteCity implements ModInitializer {
   public void onInitialize() {
     Registry.register(Registries.CHUNK_GENERATOR, new Identifier(MOD_ID, "city"), InfiniteCityChunkGenerator.CODEC);
     ModBlocks.init();
+    CustomPortalBuilder.beginPortal()
+        .frameBlock(Blocks.LIGHT_GRAY_CONCRETE)
+        .customIgnitionSource(PortalIgnitionSource.FIRE)
+        .onlyLightInOverworld()
+        .destDimID(new Identifier(MOD_ID, "city"))
+        .tintColor(100, 100, 100)
+        .registerPortal();
   }
 }
